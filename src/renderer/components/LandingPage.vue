b0f1f271621b4fee0e7417d24112cb34c74c1426
Tess Monical <tess.monical@gmail.com>
2017-11-17 10:47:29 -0600

    initial commit to github

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,136 @@
+<template>
+  <div id="wrapper">
+    <main>
+      <div class="main-col">
+        <img id="logo" src="~@/assets/cookiecat.png" alt="cookie-cat">
+        <span class="title">
+          Cookie Cat Writing App!
+        </span>
+        <inspirational-quote></inspirational-quote>
+        <textarea v-model="text" id="main-textarea" placeholder="Start typing"></textarea>
+      <p>wordcount: {{wordcount}}</p>
+      <p>text: {{text}} </p>
+      </div>
+
+    </main>
+  </div>
+</template>
+
+<script>
+  // import SystemInformation from './LandingPage/SystemInformation';
+  import InspirationalQuote from './LandingPage/InspirationalQuote';
+
+  export default {
+    components: { InspirationalQuote },
+    data() {
+      return {
+        text: 'hi',
+        name: 'landing-page',
+        methods: {
+          open(link) {
+            this.$electron.shell.openExternal(link);
+          },
+        },
+      };
+    },
+    computed: {
+      wordcount() {
+        return this.text.match(/\S+/g).length;
+      },
+    },
+  };
+
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+
+  #wrapper {
+    background:
+      linear-gradient(45deg,#ffedee,#ffefdd);
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+  }
+
+  #main-textarea {
+    height: 50vh;
+    width: 90vw;
+    padding: 10px;
+  }
+
+  #logo {
+    height: auto;
+    margin-bottom: 10px;
+    width: 150px;
+    resize: none;
+    overflow: scroll;
+  }
+
+  main {
+    display: flex;
+  }
+
+  main > div {
+    justify-content: center;
+    align-items: center;
+  }
+
+  .main-col {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
+
+  .welcome {
+    color: #555;
+    font-size: 23px;
+    margin-bottom: 10px;
+  }
+
+  .title {
+    color: #2c3e50;
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 6px;
+  }
+
+  .title.alt {
+    font-size: 18px;
+    margin-bottom: 10px;
+  }
+
+  .doc p {
+    color: black;
+    margin-bottom: 10px;
+  }
+
+  .doc button {
+    font-size: .8em;
+    cursor: pointer;
+    outline: none;
+    padding: 0.75em 2em;
+    border-radius: 2em;
+    display: inline-block;
+    color: #fff;
+    background-color: #4fc08d;
+    transition: all 0.15s ease;
+    box-sizing: border-box;
+    border: 1px solid #4fc08d;
+  }
+
+  .doc button.alt {
+    color: #42b983;
+    background-color: transparent;
+  }
+</style>
---
+++ src/renderer/components/LandingPage/InspirationalQuote.vue
@@ -0,0 +1,25 @@
+<template>
+  <div> Quote of the day: {{ quote }}</div>
+</template>
+
+<script>
+const quotes = [
+  'You are braver than you believe, and smarter than you think.',
+  'You are awesome',
+  'Butts butts butts farts',
+];
+const newQuote = () => quotes[Math.floor(Math.random() * quotes.length)];
+export default {
+  data() {
+    return {
+      quote: newQuote(),
+      name: 'inspirational-quote',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
+
